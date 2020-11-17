33cb39d4b439ffdff625fbd362603a6b52063b4f
BuildTools <unconfigured@null.spigotmc.org>
2020-11-17 12:59:35 +0100

    Small Changes

--- Server/const_var.go
+++ Server/const_var.go
@@ -1,23 +1,25 @@
 package Server
 
 import (
-	"marvin/GraphEng/GE"
-	"marvin/GameConn/GC"
 	"flag"
+	"marvin/GameConn/GC"
+	"marvin/GraphEng/GE"
 )
 
 const (
-	RES = 						"./.res"
-	STRUCTURE_FILES = 			"/Maps/structures"
-	TILE_FILES = 				"/Maps/tiles"
-	
-	MAP_REQUEST = 				GC.MESSAGE_TYPES+0
+	RES             = "./.res"
+	STRUCTURE_FILES = "/Maps/structures"
+	TILE_FILES      = "/Maps/tiles"
+
+	MAP_REQUEST = GC.MESSAGE_TYPES + 0
+	CHAR_SEND   = GC.MESSAGE_TYPES + 1
 )
+
 var (
-	port = flag.String("port", "8080", "Port of the server to run on")
+	port       = flag.String("port", "8080", "Port of the server to run on")
 	world_file = flag.String("world", "./.res/Maps/Worlds/ben.map", "path of the world that the server is going to host")
-	Server *GC.Server
-	World *GE.WorldStructure
+	Server     *GC.Server
+	World      *GE.WorldStructure
 	wrld_bytes []byte
-	done chan struct{}
+	done       chan struct{}
 )
--- Server/start.go
+++ Server/start.go
@@ -1,52 +1,60 @@
 package Server
 
 import (
-    "net"
-	"log"
-	"fmt"
 	"flag"
+	"fmt"
+	"log"
+	"net"
+
 	//"strings"
 	"marvin/GameConn/GC"
 	"marvin/GraphEng/GE"
+
 	ws "github.com/gorilla/websocket"
 )
 
 func Start() {
 	flag.Parse()
-	if flag.NFlag() != 2 {log.Fatal("Please specifiy the following flags: [port], [port]\nUse -help to display all posible flags")}
+	if flag.NFlag() != 2 {
+		log.Fatal("Please specifiy the following flags: [port], [port]\nUse -help to display all posible flags")
+	}
 	done = make(chan struct{})
-	
-	wrld, err := GE.LoadWorldStructure(0,0,1920,1080, *world_file, RES+TILE_FILES, RES+STRUCTURE_FILES)
+
+	wrld, err := GE.LoadWorldStructure(0, 0, 1920, 1080, *world_file, RES+TILE_FILES, RES+STRUCTURE_FILES)
 	CheckErr(err)
 	World = wrld
 	wrld_bytes, err = wrld.ToBytes()
 	wrld_bytes = append([]byte{MAP_REQUEST}, wrld_bytes...)
 	CheckErr(err)
-	
+
 	GC.InitSyncVarStandardTypes()
 	Server = GC.GetNewServer()
 	servermanager := GC.GetServerManager(Server)
 	servermanager.InputHandler = ServerInput
 	servermanager.OnNewConn = ServerNewConn
 	servermanager.OnCloseConn = ServerCloseConn
-	
+
 	//Runs the server
 	ipAddr := GetLocalIP()
 	ipAddrS := fmt.Sprintf("%s:%s", ipAddr, *port)
 	fmt.Println("Running on:", ipAddrS)
 	Server.Run(ipAddrS)
-	
+
 	<-done
 }
 
 func ServerInput(c *ws.Conn, mt int, msg []byte, err error, s *GC.Server) {
 	fmt.Printf("Client %s send msg of len(%v): '%v'\n", c.RemoteAddr().String(), len(msg), msg)
-	
+
 	if msg[0] == MAP_REQUEST {
 		fmt.Print("Sending map...")
 		s.Send(wrld_bytes, s.ConnToIdx[c])
 		s.WaitForConfirmation(s.ConnToIdx[c])
 		fmt.Print("done\n")
+	}
+
+	if msg[0] == CHAR_SEND {
+		fmt.Printf("%v %v %v %v \n", msg[1], msg[2], msg[3], msg[4])
 	}
 }
 func ServerNewConn(c *ws.Conn, mt int, msg []byte, err error, s *GC.Server) {
@@ -76,17 +84,17 @@ func CheckErr(err error) {
 //}
 
 func GetLocalIP() string {
-    addrs, err := net.InterfaceAddrs()
-    if err != nil {
-        return ""
-    }
-    for _, address := range addrs {
-        // check the address type and if it is not a loopback the display it
-        if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-            if ipnet.IP.To4() != nil {
-                return ipnet.IP.String()
-            }
-        }
-    }
-    return ""
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
 }
