e1b3c184388d3aa22026e1396f4b4135ba7b4b1f
Mortim-Portim <>
2021-02-27 22:25:11 +0100

    fett

--- Server/start.go
+++ Server/start.go
@@ -66,7 +66,7 @@ func Start() {
 	}
 
 	GC.InitSyncVarStandardTypes()
-	GC.PRINT_LOG_PRIORITY = 1
+	GC.PRINT_LOG_PRIORITY = 5
 	Server = GC.GetNewServer()
 	ServerManager = GC.GetServerManager(Server)
 	ServerManager.InputHandler = ServerInput
@@ -162,7 +162,6 @@ func ServerInput(c *ws.Conn, mt int, msg []byte, err error, s *GC.Server) {
 func ServerNewConn(c *ws.Conn, mt int, msg []byte, err error, s *GC.Server) {
 	fmt.Println("New Client Connected: ", c.RemoteAddr().String())
 
-	playerJoining.Lock()
 	data := append([]byte{GC.BINARYMSG}, []byte(TNE.NumberOfSVACIDs_Msg)...)
 	data = append(data, cmp.Int16ToBytes(int16(TNE.GetSVACID_Count()))...)
 	s.SendBuffered(data, c)
@@ -171,11 +170,14 @@ func ServerNewConn(c *ws.Conn, mt int, msg []byte, err error, s *GC.Server) {
 	newSM := SmallWorld.New()
 	newSM.Register(ServerManager, c)
 
+	time.Sleep(time.Millisecond * 50)
+
 	newSM.SetWorldStruct(newSM.Struct)
 	newSM.SetTimePerFrame(SmallWorld.TimePerFrame)
 	ServerManager.UpdateSyncVarsNormal()
 	s.WaitForConfirmation(c)
 
+	playerJoining.Lock()
 	SmPerCon[c] = newSM
 	PlayersChanged = true
 	playerJoining.Unlock()
